cca2084a453bb1a61fdbf43e1c591ad862b792dd
cs <bartus.csongor@gmail.com>
2015-09-14 09:41:00 +0300

    Background images are working

--- site/components/framework/structure/background-image/background-image.js
+++ site/components/framework/structure/background-image/background-image.js
@@ -1,8 +1,38 @@
+// Read the `figure` element's responsive images and breakpoints
+// Set as background image with `matchMedia` coming from `Picturefill`
 var setBackgroundImage = function(elementID) {
   var element = document.querySelector(elementID);
-  var imageName = element.getAttribute('data-image');
+  var picture = document.querySelector(elementID + ' .picture');
 
-  if (matchMedia('only screen and (min-width: 1601px)').matches) {
-    element.style.backgroundImage = "url('" + imageName + "_desktop.png')";
+  // Collect images and breakpoints from `picture`
+  // - <source media="(min-width: 600px)" srcset="/assets/images/placeholder-16-9_tablet.png, /assets/images/placeholder-16-9_tablet2x.png 2x">
+  // - start with mobile then go to desktop; media queries stop at the first match
+  for (var i = picture.children.length - 1; i >= 0 ; i--) {
+    var srcset = picture.children[i].srcset;
+    var media = picture.children[i].media;
+    var images = srcset.split(', ');
+
+    for (var j = images.length - 1; j >= 0; j--) {
+      var x2 = images[j].split(' 2x');
+      var retina = (x2.length > 1);
+
+      setResponsiveBackgroundImage(element, x2[0], media, retina);
+    }
+  }
+
+  // Set a responsive background image using `mathcMedia`
+  function setResponsiveBackgroundImage(element, image, breakpoint, retina) {
+    console.log('i:' + image + ' b:' + breakpoint + ' r:' + retina);
+    var mediaQuery = "only screen and " + breakpoint;
+    console.log('m:' + mediaQuery);
+
+    if (retina) {
+      mediaQuery += " and (-webkit-min-device-pixel-ratio: 2) and (min--moz-device-pixel-ratio: 2) and (-o-min-device-pixel-ratio: 2/1)";
+      mediaQuery += " and (min-device-pixel-ratio: 2) and (min-resolution: 192dpi) and (min-resolution: 2dppx)";
+    }
+
+    if (matchMedia(mediaQuery).matches) {
+      element.style.backgroundImage = "url('" + image + "')";
+    }
   }
 };
