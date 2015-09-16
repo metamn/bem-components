60fd0b9dc2369bf4b752cc90e6115223ed750842
cs <bartus.csongor@gmail.com>
2015-09-16 10:29:47 +0300

    Documentation added to the image loader

--- site/components/framework/structure/figure/img/--loading/img--loading.js
+++ site/components/framework/structure/figure/img/--loading/img--loading.js
@@ -1,3 +1,5 @@
+// Adding the class `img--loaded` for all images in a container after they are fully loaded
+// - uses the imagesLoaded external library
 var imageLoading = function(containerID) {
   var container = document.querySelector(containerID);
   var images = document.querySelectorAll(containerID + ' img');
