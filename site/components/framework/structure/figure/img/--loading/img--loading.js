62ee8761291dd3d5b297966b83f6c0831c026850
cs <bartus.csongor@gmail.com>
2015-09-16 10:21:18 +0300

    Images loading works for the hero

--- site/components/framework/structure/figure/img/--loading/img--loading.js
+++ site/components/framework/structure/figure/img/--loading/img--loading.js
@@ -1,13 +1,10 @@
-var showImages = function(containerID) {
+var imageLoading = function(containerID) {
   var container = document.querySelector(containerID);
   var images = document.querySelectorAll(containerID + ' img');
 
   imagesLoaded(container, function(instance) {
-    console.log('All images are loaded');
     for (var i = 0; i < images.length; i++) {
-      images[i].style.opacity = "1";
+      images[i].classList.add('img--loaded');
     }
   });
 }
-
-showImages('body');
