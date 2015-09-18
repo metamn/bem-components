f18938f1a89a5bc33069d664e6f6abc4ba5b0c66
cs <bartus.csongor@gmail.com>
2015-09-18 15:01:00 +0300

    It seems Picturefill and imagesLoaded libraries do not cope well

--- site/components/framework/structure/figure/img/--loading/img--loading.js
+++ site/components/framework/structure/figure/img/--loading/img--loading.js
@@ -11,11 +11,17 @@ var imagesLoading = function(containerID) {
 // Adding the class `img-loaded` for images inside a single container
 // - uses the imagesLoaded external library
 var imageLoading = function(container) {
+  console.log('container:' + container.id);
+
   var images = container.querySelectorAll('.img');
   var figures = container.querySelectorAll('.figure');
 
   imagesLoaded(container, function(instance) {
+    console.log('container images loaded:' + container.id);
+
     for (var i = 0; i < images.length; i++) {
+      console.log('image loaded:' + images[i].getAttribute(src));
+
       figures[i].classList.add('figure--loaded');
       images[i].classList.add('img--loaded');
     }
