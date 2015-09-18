4868607255a7f46b4dc1a96be77c31bfa8e30b02
cs <bartus.csongor@gmail.com>
2015-09-18 21:40:36 +0300

    Adding text clipping mask example

--- site/components/framework/structure/figure/img/--loading/img--loading.js
+++ site/components/framework/structure/figure/img/--loading/img--loading.js
@@ -11,17 +11,11 @@ var imagesLoading = function(containerID) {
 // Adding the class `img-loaded` for images inside a single container
 // - uses the imagesLoaded external library
 var imageLoading = function(container) {
-  console.log('container:' + container.id);
-
   var images = container.querySelectorAll('.img');
   var figures = container.querySelectorAll('.figure');
 
   imagesLoaded(container, function(instance) {
-    console.log('container images loaded:' + container.id);
-
     for (var i = 0; i < images.length; i++) {
-      console.log('image loaded:' + images[i].src);
-
       figures[i].classList.add('figure--loaded');
       images[i].classList.add('img--loaded');
     }
