e1800497f2adf0034629a3aedfe0c3a1f8dcb1da
cs <bartus.csongor@gmail.com>
2015-09-19 11:44:40 +0300

    Video fallback image set to be responsive

--- site/components/framework/structure/background-image/background-image.js
+++ site/components/framework/structure/background-image/background-image.js
@@ -1,24 +1,6 @@
 // Read the `figure` element's responsive images and breakpoints
 // Set as background image with `matchMedia` coming from `Picturefill`
 var setBackgroundImage = function(elementID) {
-  var element = document.querySelector(elementID);
-  var picture = document.querySelector(elementID + ' .picture');
-
-  // Collect images and breakpoints from `picture`
-  // - <source media="(min-width: 600px)" srcset="/assets/images/placeholder-16-9_tablet.png, /assets/images/placeholder-16-9_tablet2x.png 2x">
-  // - start with mobile then go to desktop; media queries stop at the first match
-  for (var i = picture.children.length - 1; i >= 0 ; i--) {
-    var srcset = picture.children[i].srcset;
-    var media = picture.children[i].media;
-    var images = srcset.split(', ');
-
-    for (var j = 0; j < images.length; j++) {
-      var x2 = images[j].split(' 2x');
-      var retina = (x2.length > 1);
-
-      setResponsiveBackgroundImage(element, x2[0], media, retina);
-    }
-  }
 
   // Set a responsive background image using `mathcMedia`
   function setResponsiveBackgroundImage(element, image, breakpoint, retina) {
@@ -33,4 +15,6 @@ var setBackgroundImage = function(elementID) {
       element.style.backgroundImage = "url('" + image + "')";
     }
   }
+
+  responsiveImageURL(elementID, setResponsiveBackgroundImage);
 };
---
+++ site/components/project/video-example/video-example.js
@@ -0,0 +1,1 @@
+videoResponsivePoster('.video-example');
